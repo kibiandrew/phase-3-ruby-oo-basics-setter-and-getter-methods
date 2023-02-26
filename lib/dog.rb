11124a42e48ce8074398dd0944a1881ea8a103cd
Andrew Kibiwott <kibiandrew@gmail.com>
2023-02-26 22:22:29 +0300

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
 
+    def breed=(breed)
+    @breed=breed
+    end
+
+    def name
+    @name
+    end
+
+    def breed
+    @breed 
+    end
 end
+
+lassie=Dog.new
+lassie.name= "Lassie"
+lassie.breed= "Collie"
+
+puts lassie.name
+#=> Lassie
+puts lassie.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-
-end
+    def name=(person_names)
+        @name=person_names
+    end
+    def job=(person_jobs)
+        @job=person_jobs
+    end
+    def name
+        @name
+      end
+      def job
+        @job
+      end
+    end
+    drew=Person.new
+    drew.name="drew"
+    drew.job="Software Developer"
+    
+    puts drew.name
+    puts drew.job
+    
